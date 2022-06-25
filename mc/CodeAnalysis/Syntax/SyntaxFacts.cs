bec9cb242ec58cae47ca626737a9e71855fd5649
alan-matthew <alan.matthew.anggara@gmail.com>
2022-06-26 04:43:07 +0800

    add support for == and !=

--- mc/CodeAnalysis/Syntax/SyntaxFacts.cs
+++ mc/CodeAnalysis/Syntax/SyntaxFacts.cs
@@ -10,7 +10,8 @@ namespace Minsk.CodeAnalysis.Syntax
             {
                 case SyntaxKind.PlusToken:
                 case SyntaxKind.MinusToken:
-                    return 3;
+                case SyntaxKind.BangToken:
+                    return 6;
 
                 default:
                     return 0;
@@ -23,10 +24,20 @@ namespace Minsk.CodeAnalysis.Syntax
             {
                 case SyntaxKind.StarToken:
                 case SyntaxKind.SlashToken:
-                    return 2;
+                    return 5;
 
                 case SyntaxKind.PlusToken:
                 case SyntaxKind.MinusToken:
+                    return 4;
+
+                case SyntaxKind.EqualsEqualsToken:
+                case SyntaxKind.BangEqualsToken:
+                    return 3;
+
+                case SyntaxKind.AmpersandAmpersandToken:
+                    return 2;
+
+                case SyntaxKind.PipePipeToken:
                     return 1;
 
                 default:
