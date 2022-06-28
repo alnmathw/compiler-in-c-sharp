e57b7178b03402edd822af0ea13726a98974fed0
alan-matthew <alan.matthew.anggara@gmail.com>
2022-06-28 15:57:24 +0800

    adding several Evaluator tests

--- src/Minsk.Tests/CodeAnalysis/EvaluationTests.cs
+++ src/Minsk.Tests/CodeAnalysis/EvaluationTests.cs
@@ -69,6 +69,7 @@ namespace Minsk.Tests.CodeAnalysis
         [InlineData("\"test\" != \"test\"", false)]
         [InlineData("\"test\" == \"abc\"", false)]
         [InlineData("\"test\" != \"abc\"", true)]
+        [InlineData("\"test\" + \"abc\"", "testabc")]
         [InlineData("{ var a = 10 (a * a) }", 100)]
         [InlineData("{ var a = 0 (a = 10) * a }", 100)]
         [InlineData("{ var a = 0 if a == 0 a = 10 a }", 10)]
@@ -79,6 +80,8 @@ namespace Minsk.Tests.CodeAnalysis
         [InlineData("{ var result = 0 for i = 1 to 10 { result = result + i } result }", 55)]
         [InlineData("{ var a = 10 for i = 1 to (a = a - 1) { } a }", 9)]
         [InlineData("{ var a = 0 do a = a + 1 while a < 10 a}", 10)]
+        [InlineData("{ var i = 0 while i < 5 { i = i + 1 if i == 5 continue } i }", 5)]
+        [InlineData("{ var i = 0 do { i = i + 1 if i == 5 continue } while i < 5 i }", 5)]
         public void Evaluator_Computes_CorrectValues(string text, object expectedValue)
         {
             AssertValue(text, expectedValue);
@@ -383,6 +386,172 @@ namespace Minsk.Tests.CodeAnalysis
 
             var diagnostics = @"
                 Function 'print' doesn't exist.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Void_Function_Should_Not_Return_Value()
+        {
+            var text = @"
+                function test()
+                {
+                    return [1]
+                }
+            ";
+
+            var diagnostics = @"
+                Since the function 'test' does not return a value the 'return' keyword cannot be followed by an expression.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Function_With_ReturnValue_Should_Not_Return_Void()
+        {
+            var text = @"
+                function test(): int
+                {
+                    [return]
+                }
+            ";
+
+            var diagnostics = @"
+                An expression of type 'int' expected.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Not_All_Code_Paths_Return_Value()
+        {
+            var text = @"
+                function [test](n: int): bool
+                {
+                    if (n > 10)
+                       return true
+                }
+            ";
+
+            var diagnostics = @"
+                Not all code paths return a value.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Expression_Must_Have_Value()
+        {
+            var text = @"
+                function test(n: int)
+                {
+                    return
+                }
+
+                let value = [test(100)]
+            ";
+
+            var diagnostics = @"
+                Expression must have a value.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Theory]
+        [InlineData("[break]", "break")]
+        [InlineData("[continue]", "continue")]
+        public void Evaluator_Invalid_Break_Or_Continue(string text, string keyword)
+        {
+            var diagnostics = $@"
+                The keyword '{keyword}' can only be used inside of loops.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Invalid_Return()
+        {
+            var text = @"
+                [return]
+            ";
+
+            var diagnostics = @"
+                The 'return' keyword can only be used inside of functions.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Parameter_Already_Declared()
+        {
+            var text = @"
+                function sum(a: int, b: int, [a: int]): int
+                {
+                    return a + b + c
+                }
+            ";
+
+            var diagnostics = @"
+                A parameter with the name 'a' already exists.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Function_Must_Have_Name()
+        {
+            var text = @"
+                function [(]a: int, b: int): int
+                {
+                    return a + b
+                }
+            ";
+
+            var diagnostics = @"
+                Unexpected token <OpenParenthesisToken>, expected <IdentifierToken>.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Wrong_Argument_Type()
+        {
+            var text = @"
+                function test(n: int): bool
+                {
+                    return n > 10
+                }
+                let testValue = ""string""
+                test([testValue])
+            ";
+
+            var diagnostics = @"
+                Parameter 'n' requires a value of type 'int' but was given a value of type 'string'.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Bad_Type()
+        {
+            var text = @"
+                function test(n: [invalidtype])
+                {
+                }
+            ";
+
+            var diagnostics = @"
+                Type 'invalidtype' doesn't exist.
             ";
 
             AssertDiagnostics(text, diagnostics);
