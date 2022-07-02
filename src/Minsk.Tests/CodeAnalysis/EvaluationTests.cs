8b1eec832d82c993dcf64b0e03b9641b24da6521
alan-matthew <alan.matthew.anggara@gmail.com>
2022-07-02 18:48:19 +0800

    add support for -=, *=, /=, &=, |= and ^=

--- src/Minsk.Tests/CodeAnalysis/EvaluationTests.cs
+++ src/Minsk.Tests/CodeAnalysis/EvaluationTests.cs
@@ -62,43 +62,28 @@ namespace Minsk.Tests.CodeAnalysis
         [InlineData("false", false)]
         [InlineData("!true", false)]
         [InlineData("!false", true)]
-        [InlineData("var a = 10 return a", 10)]
-        [InlineData("\"test\"", "test")]
-        [InlineData("\"te\"\"st\"", "te\"st")]
-        [InlineData("\"test\" == \"test\"", true)]
-        [InlineData("\"test\" != \"test\"", false)]
-        [InlineData("\"test\" == \"abc\"", false)]
-        [InlineData("\"test\" != \"abc\"", true)]
-        [InlineData("\"test\" + \"abc\"", "testabc")]
-        [InlineData("{ var a : any = 0 var b : any = \"b\" return a == b }", false)]
-        [InlineData("{ var a : any = 0 var b : any = \"b\" return a != b }", true)]
-        [InlineData("{ var a : any = 0 var b : any = 0 return a == b }", true)]
-        [InlineData("{ var a : any = 0 var b : any = 0 return a != b }", false)]
-        [InlineData("{ var a = 10 return a * a }", 100)]
-        [InlineData("{ var a = 0 return (a = 10) * a }", 100)]
-        [InlineData("{ var a = 0 if a == 0 a = 10 return a }", 10)]
-        [InlineData("{ var a = 0 if a == 4 a = 10 return a }", 0)]
-        [InlineData("{ var a = 0 if a == 0 a = 10 else a = 5 return a }", 10)]
-        [InlineData("{ var a = 0 if a == 4 a = 10 else a = 5 return a }", 5)]
-        [InlineData("{ var i = 10 var result = 0 while i > 0 { result = result + i i = i - 1} return result }", 55)]
-        [InlineData("{ var result = 0 for i = 1 to 10 { result = result + i } return result }", 55)]
-        [InlineData("{ var a = 10 for i = 1 to (a = a - 1) { } return a }", 9)]
-        [InlineData("{ var a = 0 do a = a + 1 while a < 10 return a}", 10)]
-        [InlineData("{ var i = 0 while i < 5 { i = i + 1 if i == 5 continue } return i }", 5)]
-        [InlineData("{ var i = 0 do { i = i + 1 if i == 5 continue } while i < 5 return i }", 5)]
-        [InlineData("{ var a = 1 a += (2 + 3) return a }", 6)]
-        [InlineData("{ var a = 1 a -= (2 + 3) return a }", -4)]
-        [InlineData("{ var a = 1 a *= (2 + 3) return a }", 5)]
-        [InlineData("{ var a = 1 a /= (2 + 3) return a }", 0)]
-        [InlineData("{ var a = true a &= (false) return a }", false)]
-        [InlineData("{ var a = true a |= (false) return a }", true)]
-        [InlineData("{ var a = true a ^= (true) return a }", false)]
-        [InlineData("{ var a = 1 a |= 0 return a }", 1)]
-        [InlineData("{ var a = 1 a &= 3 return a }", 1)]
-        [InlineData("{ var a = 1 a &= 0 return a }", 0)]
-        [InlineData("{ var a = 1 a ^= 0 return a }", 1)]
-        [InlineData("{ var a = 1 var b = 2 var c = 3 a += b += c return a }", 6)]
-        [InlineData("{ var a = 1 var b = 2 var c = 3 a += b += c return b }", 5)]
+        [InlineData("var a = 10", 10)]
+        [InlineData("{ var a = 10 (a * a) }", 100)]
+        [InlineData("{ var a = 0 (a = 10) * a }", 100)]
+        [InlineData("{ var a = 0 if a == 0 a = 10 a }", 10)]
+        [InlineData("{ var a = 0 if a == 4 a = 10 a }", 0)]
+        [InlineData("{ var a = 0 if a == 0 a = 10 else a = 5 a }", 10)]
+        [InlineData("{ var a = 0 if a == 4 a = 10 else a = 5 a }", 5)]
+        [InlineData("{ var i = 10 var result = 0 while i > 0 { result = result + i i = i - 1} result }", 55)]
+        [InlineData("{ var result = 0 for i = 1 to 10 { result = result + i } result }", 55)]
+        [InlineData("{ var a = 10 for i = 1 to (a = a - 1) { } a }", 9)]
+        [InlineData("{ var a = 0 do a = a + 1 while a < 10 a}", 10)]
+        [InlineData("{ var a = 1 a += (2 + 3) a }", 6)]
+        [InlineData("{ var a = 1 a -= (2 + 3) a }", -4)]
+        [InlineData("{ var a = 1 a *= (2 + 3) a }", 5)]
+        [InlineData("{ var a = 1 a /= (2 + 3) a }", 0)]
+        [InlineData("{ var a = true a &= (false) a }", false)]
+        [InlineData("{ var a = true a |= (false) a }", true)]
+        [InlineData("{ var a = true a ^= (true) a }", false)]
+        [InlineData("{ var a = 1 a|= 0 a }", 1)]
+        [InlineData("{ var a = 1 a&= 3 a }", 1)]
+        [InlineData("{ var a = 1 a&= 0 a }", 0)]
+        [InlineData("{ var a = 1 a^= 0 a }", 1)]
         public void Evaluator_Computes_CorrectValues(string text, object expectedValue)
         {
             AssertValue(text, expectedValue);
@@ -136,87 +121,6 @@ namespace Minsk.Tests.CodeAnalysis
             var diagnostics = @"
                 Unexpected token <CloseParenthesisToken>, expected <IdentifierToken>.
                 Unexpected token <EndOfFileToken>, expected <CloseBraceToken>.
-            ";
-
-            AssertDiagnostics(text, diagnostics);
-        }
-
-        [Fact]
-        public void Evaluator_InvokeFunctionArguments_Missing()
-        {
-            var text = @"
-                print([)]
-            ";
-
-            var diagnostics = @"
-                Function 'print' requires 1 arguments but was given 0.
-            ";
-
-            AssertDiagnostics(text, diagnostics);
-        }
-
-        [Fact]
-        public void Evaluator_InvokeFunctionArguments_Exceeding()
-        {
-            var text = @"
-                print(""Hello""[, "" "", "" world!""])
-            ";
-
-            var diagnostics = @"
-                Function 'print' requires 1 arguments but was given 3.
-            ";
-
-            AssertDiagnostics(text, diagnostics);
-        }
-
-        [Fact]
-        public void Evaluator_InvokeFunctionArguments_NoInfiniteLoop()
-        {
-            var text = @"
-                print(""Hi""[[=]][)]
-            ";
-
-            var diagnostics = @"
-                Unexpected token <EqualsToken>, expected <CloseParenthesisToken>.
-                Unexpected token <EqualsToken>, expected <IdentifierToken>.
-                Unexpected token <CloseParenthesisToken>, expected <IdentifierToken>.
-            ";
-
-            AssertDiagnostics(text, diagnostics);
-        }
-
-        [Fact]
-        public void Evaluator_FunctionParameters_NoInfiniteLoop()
-        {
-            var text = @"
-                function hi(name: string[[[=]]][)]
-                {
-                    print(""Hi "" + name + ""!"" )
-                }[]
-            ";
-
-            var diagnostics = @"
-                Unexpected token <EqualsToken>, expected <CloseParenthesisToken>.
-                Unexpected token <EqualsToken>, expected <OpenBraceToken>.
-                Unexpected token <EqualsToken>, expected <IdentifierToken>.
-                Unexpected token <CloseParenthesisToken>, expected <IdentifierToken>.
-                Unexpected token <EndOfFileToken>, expected <CloseBraceToken>.
-            ";
-
-            AssertDiagnostics(text, diagnostics);
-        }
-
-        [Fact]
-        public void Evaluator_FunctionReturn_Missing()
-        {
-            var text = @"
-                function [add](a: int, b: int): int
-                {
-                }
-            ";
-
-            var diagnostics = @"
-                Not all code paths return a value.
             ";
 
             AssertDiagnostics(text, diagnostics);
@@ -362,9 +266,9 @@ namespace Minsk.Tests.CodeAnalysis
         }
 
         [Fact]
-        public void Evaluator_CompoundExpression_Reports_Undefined()
+        public void Evaluator_LambdaDeclarationExpression_Reports_Undefined()
         {
-            var text = @"var x = 10
+            var text = @"var x = 10 
                          x [+=] false";
 
             var diagnostics = @"
@@ -387,24 +291,12 @@ namespace Minsk.Tests.CodeAnalysis
         }
 
          [Fact]
-        public void Evaluator_CompoundExpression_Assignemnt_NonDefinedVariable_Reports_Undefined()
+        public void Evaluator_LambdaDeclarationExpression_Assignemnt_NonDefinedVariable_Reports_Undefined()
         {
             var text = @"[x] += 10";
 
             var diagnostics = @"
                 Variable 'x' doesn't exist.
-            ";
-
-            AssertDiagnostics(text, diagnostics);
-        }
-
-        [Fact]
-        public void Evaluator_AssignmentExpression_Reports_NotAVariable()
-        {
-            var text = @"[print] = 42";
-
-            var diagnostics = @"
-                'print' is not a variable.
             ";
 
             AssertDiagnostics(text, diagnostics);
@@ -428,7 +320,7 @@ namespace Minsk.Tests.CodeAnalysis
         }
 
          [Fact]
-        public void Evaluator_CompoundDeclarationExpression_Reports_CannotAssign()
+        public void Evaluator_LambdaDeclarationExpression_Reports_CannotAssign()
         {
             var text = @"
                 {
@@ -462,35 +354,6 @@ namespace Minsk.Tests.CodeAnalysis
         }
 
         [Fact]
-        public void Evaluator_CallExpression_Reports_Undefined()
-        {
-            var text = @"[foo](42)";
-
-            var diagnostics = @"
-                Function 'foo' doesn't exist.
-            ";
-
-            AssertDiagnostics(text, diagnostics);
-        }
-
-        [Fact]
-        public void Evaluator_CallExpression_Reports_NotAFunction()
-        {
-            var text = @"
-                {
-                    let foo = 42
-                    [foo](42)
-                }
-            ";
-
-            var diagnostics = @"
-                'foo' is not a function.
-            ";
-
-            AssertDiagnostics(text, diagnostics);
-        }
-
-        [Fact]
         public void Evaluator_Variables_Can_Shadow_Functions()
         {
             var text = @"
@@ -501,237 +364,7 @@ namespace Minsk.Tests.CodeAnalysis
             ";
 
             var diagnostics = @"
-                'print' is not a function.
-            ";
-
-            AssertDiagnostics(text, diagnostics);
-        }
-
-        [Fact]
-        public void Evaluator_Void_Function_Should_Not_Return_Value()
-        {
-            var text = @"
-                function test()
-                {
-                    return [1]
-                }
-            ";
-
-            var diagnostics = @"
-                Since the function 'test' does not return a value the 'return' keyword cannot be followed by an expression.
-            ";
-
-            AssertDiagnostics(text, diagnostics);
-        }
-
-        [Fact]
-        public void Evaluator_Function_With_ReturnValue_Should_Not_Return_Void()
-        {
-            var text = @"
-                function test(): int
-                {
-                    [return]
-                }
-            ";
-
-            var diagnostics = @"
-                An expression of type 'int' is expected.
-            ";
-
-            AssertDiagnostics(text, diagnostics);
-        }
-
-        [Fact]
-        public void Evaluator_Not_All_Code_Paths_Return_Value()
-        {
-            var text = @"
-                function [test](n: int): bool
-                {
-                    if (n > 10)
-                       return true
-                }
-            ";
-
-            var diagnostics = @"
-                Not all code paths return a value.
-            ";
-
-            AssertDiagnostics(text, diagnostics);
-        }
-
-        [Fact]
-        public void Evaluator_Expression_Must_Have_Value()
-        {
-            var text = @"
-                function test(n: int)
-                {
-                    return
-                }
-
-                let value = [test(100)]
-            ";
-
-            var diagnostics = @"
-                Expression must have a value.
-            ";
-
-            AssertDiagnostics(text, diagnostics);
-        }
-
-        [Fact]
-        public void Evaluator_IfStatement_Reports_NotReachableCode_Warning()
-        {
-            var text = @"
-                function test()
-                {
-                    let x = 4 * 3
-                    if x > 12
-                    {
-                        [print](""x"")
-                    }
-                    else
-                    {
-                        print(""x"")
-                    }
-                }
-            ";
-
-            var diagnostics = @"
-                Unreachable code detected.
-            ";
-            AssertDiagnostics(text, diagnostics, assertWarnings: true);
-        }
-
-        [Fact]
-        public void Evaluator_ElseStatement_Reports_NotReachableCode_Warning()
-        {
-            var text = @"
-                function test(): int
-                {
-                    if true
-                    {
-                        return 1
-                    }
-                    else
-                    {
-                        [return] 0
-                    }
-                }
-            ";
-
-            var diagnostics = @"
-                Unreachable code detected.
-            ";
-
-            AssertDiagnostics(text, diagnostics, assertWarnings: true);
-        }
-
-        [Fact]
-        public void Evaluator_WhileStatement_Reports_NotReachableCode_Warning()
-        {
-            var text = @"
-                function test()
-                {
-                    while false
-                    {
-                        [continue]
-                    }
-                }
-            ";
-
-            var diagnostics = @"
-                Unreachable code detected.
-            ";
-
-            AssertDiagnostics(text, diagnostics, assertWarnings: true);
-        }
-
-        [Theory]
-        [InlineData("[break]", "break")]
-        [InlineData("[continue]", "continue")]
-        public void Evaluator_Invalid_Break_Or_Continue(string text, string keyword)
-        {
-            var diagnostics = $@"
-                The keyword '{keyword}' can only be used inside of loops.
-            ";
-
-            AssertDiagnostics(text, diagnostics);
-        }
-
-        [Fact]
-        public void Evaluator_Script_Return()
-        {
-            var text = @"
-                return
-            ";
-
-            AssertValue(text, "");
-        }
-
-        [Fact]
-        public void Evaluator_Parameter_Already_Declared()
-        {
-            var text = @"
-                function sum(a: int, b: int, [a: int]): int
-                {
-                    return a + b + c
-                }
-            ";
-
-            var diagnostics = @"
-                A parameter with the name 'a' already exists.
-            ";
-
-            AssertDiagnostics(text, diagnostics);
-        }
-
-        [Fact]
-        public void Evaluator_Function_Must_Have_Name()
-        {
-            var text = @"
-                function [(]a: int, b: int): int
-                {
-                    return a + b
-                }
-            ";
-
-            var diagnostics = @"
-                Unexpected token <OpenParenthesisToken>, expected <IdentifierToken>.
-            ";
-
-            AssertDiagnostics(text, diagnostics);
-        }
-
-        [Fact]
-        public void Evaluator_Wrong_Argument_Type()
-        {
-            var text = @"
-                function test(n: int): bool
-                {
-                    return n > 10
-                }
-                let testValue = ""string""
-                test([testValue])
-            ";
-
-            var diagnostics = @"
-                Cannot convert type 'string' to 'int'. An explicit conversion exists (are you missing a cast?)
-            ";
-
-            AssertDiagnostics(text, diagnostics);
-        }
-
-        [Fact]
-        public void Evaluator_Bad_Type()
-        {
-            var text = @"
-                function test(n: [invalidtype])
-                {
-                }
-            ";
-
-            var diagnostics = @"
-                Type 'invalidtype' doesn't exist.
+                Function 'print' doesn't exist.
             ";
 
             AssertDiagnostics(text, diagnostics);
@@ -740,19 +373,19 @@ namespace Minsk.Tests.CodeAnalysis
         private static void AssertValue(string text, object expectedValue)
         {
             var syntaxTree = SyntaxTree.Parse(text);
-            var compilation = Compilation.CreateScript(null, syntaxTree);
+            var compilation = new Compilation(syntaxTree);
             var variables = new Dictionary<VariableSymbol, object>();
             var result = compilation.Evaluate(variables);
 
-            Assert.Empty(result.ErrorDiagnostics);
+            Assert.Empty(result.Diagnostics);
             Assert.Equal(expectedValue, result.Value);
         }
 
-        private void AssertDiagnostics(string text, string diagnosticText, bool assertWarnings = false)
+        private void AssertDiagnostics(string text, string diagnosticText)
         {
             var annotatedText = AnnotatedText.Parse(text);
             var syntaxTree = SyntaxTree.Parse(annotatedText.Text);
-            var compilation = Compilation.CreateScript(null, syntaxTree);
+            var compilation = new Compilation(syntaxTree);
             var result = compilation.Evaluate(new Dictionary<VariableSymbol, object>());
 
             var expectedDiagnostics = AnnotatedText.UnindentLines(diagnosticText);
@@ -760,17 +393,16 @@ namespace Minsk.Tests.CodeAnalysis
             if (annotatedText.Spans.Length != expectedDiagnostics.Length)
                 throw new Exception("ERROR: Must mark as many spans as there are expected diagnostics");
 
-            var diagnostics = assertWarnings ? result.Diagnostics : result.ErrorDiagnostics;
-            Assert.Equal(expectedDiagnostics.Length, diagnostics.Length);
+            Assert.Equal(expectedDiagnostics.Length, result.Diagnostics.Length);
 
             for (var i = 0; i < expectedDiagnostics.Length; i++)
             {
                 var expectedMessage = expectedDiagnostics[i];
-                var actualMessage = diagnostics[i].Message;
+                var actualMessage = result.Diagnostics[i].Message;
                 Assert.Equal(expectedMessage, actualMessage);
 
                 var expectedSpan = annotatedText.Spans[i];
-                var actualSpan = diagnostics[i].Location.Span;
+                var actualSpan = result.Diagnostics[i].Span;
                 Assert.Equal(expectedSpan, actualSpan);
             }
         }
